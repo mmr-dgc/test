78c8ecd81a579abcd155b5ec38b6e7105c047c1d
Mamoru Deguchi <mmr.deguchi@gmail.com>
2021-09-28 14:11:07 +0900

    initial

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +22,94 @@ public class SampleController {
         return service.select("my_seq");
     }
 	
-	@GetMapping("/update")
-    String update() {
+	@GetMapping("/clear")
+    String clear() {
 		SequenceEntity sequenceEntity = new SequenceEntity();
 		sequenceEntity.setName("my_seq");
-		sequenceEntity.setNext_value(20);
+		sequenceEntity.setNext_value(1);
 		service.update(sequenceEntity);
         return "Success";
+    }
+	
+	@GetMapping("/test1")
+    String test1() {
+		try {
+			return service.test1();
+		} catch (SQLTimeoutException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+    }
+	
+	@GetMapping("/test2")
+    String test2() {
+		try {
+			return service.test2();
+		} catch (NoSuchElementException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+    }
+	
+	@GetMapping("/test3")
+    String test3() {
+		try {
+			return service.test3();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+    }
+	
+	@GetMapping("/test4")
+    String test4() {
+		return service.test4();
+    }
+	
+	@GetMapping("/test5")
+    String test5() {
+		return service.test5();
+    }
+	
+	@GetMapping("/test6")
+    String test6() {
+		return service.test6();
+    }
+	
+	@GetMapping("/show")
+    String show() {
+		try {
+			return service.show();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+    }
+	
+	@GetMapping("/show2")
+    String show2() {
+		try {
+			return service.show2();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+    }
+	
+	@GetMapping("/set")
+    String set() {
+		try {
+			return service.set();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
     }
 	
 	@GetMapping("/sync")
--- src/main/java/com/example/demo/service/SampleService.java
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -3,17 +3,32 @@ package com.example.demo.service;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.SequenceEntity;
 import com.example.demo.repository.SampleRepository;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.SQLTimeoutException;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Service
 public class SampleService {
 
 	@Autowired
 	SampleRepository repository;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private HikariDataSource datasource;
 	
 	String SEQUENCE_NAME_COLUMN = "my_seq";
 	long INCREMENT_BY = 1;
@@ -41,6 +56,122 @@ public class SampleService {
 		return "Success";
 	}
 	
+	@Transactional(rollbackFor=Exception.class)
+	public String test1() throws SQLTimeoutException {
+		getNext();
+		throw new SQLTimeoutException();
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String test2() {
+		getNext();
+		throw new NoSuchElementException();
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String test3() throws SQLException {
+		getNext();
+		throw new SQLException();
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String test4(){
+		getNext();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return "success";
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String test5(){
+		jdbcTemplate.execute("SET STATEMENT_TIMEOUT = '1ms'");
+		jdbcTemplate.setQueryTimeout(1);
+		return ""+jdbcTemplate.getQueryTimeout();
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String test6() {
+		getNext();
+		error();
+		return "test6";
+	}
+	
+	public String set() throws SQLException {
+
+//		String temp = "set";
+//		try (Connection connection =
+//		    DriverManager.getConnection("jdbc:cloudspanner:/projects/tribal-mapper-326407/instances/test/databases/test")) 
+//		{			
+//		  try (Statement statement = connection.createStatement()) {
+//			statement.execute("SET STATEMENT_TIMEOUT = '3s'");
+//		  }
+//		}
+//		return temp;
+		
+		String temp = datasource.getDataSourceClassName() + " " + datasource.getDriverClassName();
+		try (Connection connection = datasource.getConnection()) 
+		{			
+		  try (Statement statement = connection.createStatement()) {
+			statement.execute("SET STATEMENT_TIMEOUT = '3s'");
+		  }
+		}
+		return temp;
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String show() throws SQLException {
+
+		String temp = "null";
+		try (Connection connection = datasource.getConnection()) 
+		{			
+		  try (Statement statement = connection.createStatement()) {
+			statement.execute("Update sequences SET next_value = 100 WHERE name = \"my_seq\"");
+			statement.execute("SET STATEMENT_TIMEOUT = '1ms'");
+		    try (ResultSet rs = statement.executeQuery("SHOW VARIABLE STATEMENT_TIMEOUT")) {
+		      while (rs.next()) {
+		    	temp = "Connected to Cloud Spanner at " + rs.getString(1);
+		        System.out.println(temp);
+		      }
+		    }
+		    Thread.sleep(1000);
+		    statement.execute("Update sequences SET next_value = 200 WHERE name = \"my_seq\"");
+		  } catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		  }
+		}
+		return temp;
+	}
+	
+	@Transactional(rollbackFor=Exception.class)
+	public String show2() throws SQLException {
+
+		String temp = "null";
+		try (Connection connection = datasource.getConnection()) 
+		{			
+		  try (Statement statement = connection.createStatement()) {
+			statement.execute("Update sequences SET next_value = 100 WHERE name = \"my_seq\"");
+			statement.execute("SET STATEMENT_TIMEOUT = '1s'");
+		    try (ResultSet rs = statement.executeQuery("SHOW VARIABLE STATEMENT_TIMEOUT")) {
+		      while (rs.next()) {
+		    	temp = "Connected to Cloud Spanner at " + rs.getString(1);
+		        System.out.println(temp);
+		      }
+		    }
+		    Thread.sleep(1000);
+		    statement.execute("Update sequences SET next_value = 200 WHERE name = \"my_seq\"");
+		  } catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		  }
+		}
+		return temp;
+	}
+	
 	public long getNext() {
 		SequenceEntity sequenceEntity = repository.select(SEQUENCE_NAME_COLUMN);
 		if(sequenceEntity == null) {
@@ -52,14 +183,15 @@ public class SampleService {
 		return value;
 	}
 	
-	@Transactional
-	public long getAndIncrementNextValueInDB() {
+	public long error() {
 		SequenceEntity sequenceEntity = repository.select(SEQUENCE_NAME_COLUMN);
 		if(sequenceEntity == null) {
 			throw new NoSuchElementException();
 		}
 		long value = sequenceEntity.getNext_value();
+		sequenceEntity.setName("error");
 		sequenceEntity.setNext_value(value + INCREMENT_BY);
+		System.out.println(sequenceEntity.getName());
 		repository.update(sequenceEntity);
 		return value;
 	}
